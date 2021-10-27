783ee8f1b4d1c99087e70820cf0b3a81d4762f19
Vladyslav Svitana <vladik719@gmail.com>
2021-10-27 19:12:11 +0200

    slider addition

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,8 @@
   const mobileMenuRef = document.querySelector("[data-menu]");
   const mobileBtnClose = document.querySelector("[data-menu-close]");
 
+  const body = document.querySelector('body');
+
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
   });
@@ -10,4 +12,61 @@
   mobileBtnClose.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
   });
+
+  handlerEscModalClose();
 })();
+
+
+
+// Реализация закрытия модалки при клике в бекдроп
+const menuBtnRef = document.querySelector('[data-menu-button]');
+const backdrop = document.querySelector('.backdrop');
+const body = document.querySelector('body');
+backdrop.addEventListener('click', onBackdropClick);
+function onBackdropClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    onModalClose();
+  }
+}
+
+// Функция закрытия модального окна при нажатии на Esc
+function handlerEscModalClose() {
+  window.addEventListener('keydown', onEsckeyClick);
+  function onEsckeyClick(evt) {
+    if (evt.code === 'Escape') {
+      onModalClose();
+    }
+  }
+}
+// Функция закрытия модального окна и снятия всех классов
+function onModalClose() {
+  backdrop.classList.add('is-hidden');
+  menuBtnRef.classList.remove('is-open');
+
+  body.classList.remove('modal-open');
+
+  // menuBtnRef.addEventListener("click", () => {
+  //   mobileMenuRef.classList.toggle("is-open");
+  // });
+}
+
+// закрытие модального окна при клике по элементам навигации
+const mainModalMenuListItems = document.querySelector('.mobile-menu__link-list');
+mainModalMenuListItems.addEventListener('click', onMainMenuItemsClick);
+
+
+function onMainMenuItemsClick(evt) {
+  if (!evt.target.classList.value === 'mobile-menu__list-item') {
+    return;
+  }
+  onModalClose();
+}
+
+const onlineBtn = document.querySelector('.contact-us__online');
+onlineBtn.addEventListener('click', onOnlineBtnClick);
+function onOnlineBtnClick(evt) {
+  if (!evt.target.classList.value === 'contact-us__online') {
+    return;
+  }
+  onModalClose();
+}
---
+++ js/slider.js
@@ -0,0 +1,108 @@
+// установка первого слайда открытым
+const sliderFirstItem = document.querySelector('.slide-1');
+const sliderBtnsList = document.querySelector('.hero-section__buttons');
+const sliderRadioList = document.querySelector('.slider-lines__form');
+
+sliderFirstItem.classList.replace('hidden', 'active-slide');
+document.querySelector('#first-slider-btn').checked = true;
+
+// слайдер - переключение через радио-кнопки
+sliderRadioList.addEventListener('click', onSideSliderNavClick);
+
+// слайдер - переключение кнопками назад и вперед 
+sliderBtnsList.addEventListener('click', onSliderBtnClick);
+
+
+
+function onSideSliderNavClick(e) {
+    const sliderFirstItem = document.querySelector('.slide-1');
+    const sliderSecondItem = document.querySelector('.slide-2');
+    const sliderThirdItem = document.querySelector('.slide-3');
+    const sliderBtnNext = document.querySelector('[data-btn-next]');
+    const sliderBtnPrev = document.querySelector('[data-btn-prev]');
+    const sliderItems = document.querySelectorAll('[data-item]');
+
+    if (e.target.nodeName !== 'SPAN') return;
+
+    sliderItems.forEach(item => {
+        item.classList.replace('active-slide', 'hidden');
+    });
+
+    switch (e.target.dataset.sliderBtn) {
+        case 'first':
+            sliderFirstItem.classList.replace('hidden', 'active-slide');
+            e.target.previousElementSibling.checked = true;
+            sliderBtnNext.disabled = false;
+            sliderBtnPrev.disabled = true;
+            break;
+        case 'second':
+            sliderSecondItem.classList.replace('hidden', 'active-slide');
+            e.target.previousElementSibling.checked = true;
+            sliderBtnNext.disabled = false;
+            sliderBtnPrev.disabled = false;
+            break;
+        case 'third':
+            sliderThirdItem.classList.replace('hidden', 'active-slide');
+            e.target.previousElementSibling.checked = true;
+            sliderBtnNext.disabled = true;
+            sliderBtnPrev.disabled = false;
+            break;
+    }
+}
+
+
+function onSliderBtnClick(e) {
+    if (e.target.nodeName !== 'BUTTON') return;
+
+
+    const sliderFirstItem = document.querySelector('.slide-1');
+    const sliderSecondItem = document.querySelector('.slide-2');
+    const sliderThirdItem = document.querySelector('.slide-3');
+    const sliderBtnNext = document.querySelector('[data-btn-next]');
+    const sliderBtnPrev = document.querySelector('[data-btn-prev]');
+    const activeSlide = document.querySelector('.active-slide');
+    const firstRadioBtn = document.querySelector('#first-slider-btn');
+    const secondRadioBtn = document.querySelector('#second-slider-btn');
+    const thirdRadioBtn = document.querySelector('#third-slider-btn');
+    const nextActiveSlide = activeSlide.nextElementSibling;
+    const preActiveSlide = activeSlide.previousElementSibling;
+
+    if (e.target === sliderBtnNext) {
+        if (sliderFirstItem === activeSlide) {
+            sliderBtnNext.disabled = false;
+            sliderBtnPrev.disabled = false;
+            secondRadioBtn.checked = true;
+        }
+
+        if (sliderSecondItem === activeSlide) {
+            sliderBtnNext.disabled = true;
+            sliderBtnPrev.disabled = false;
+            thirdRadioBtn.checked = true;
+        }
+
+        if (sliderThirdItem === activeSlide) return;
+
+        activeSlide.classList.replace('active-slide', 'hidden');
+        nextActiveSlide.classList.replace('hidden', 'active-slide');
+    }
+
+    if (e.target === sliderBtnPrev) {
+        if (sliderFirstItem === activeSlide) return;
+
+        if (sliderSecondItem === activeSlide) {
+            sliderBtnNext.disabled = false;
+            sliderBtnPrev.disabled = true;
+            firstRadioBtn.checked = true;
+        }
+
+        if (sliderThirdItem === activeSlide) {
+            sliderBtnNext.disabled = false;
+            sliderBtnPrev.disabled = false;
+            secondRadioBtn.checked = true;
+        }
+
+        activeSlide.classList.replace('active-slide', 'hidden');
+        preActiveSlide.classList.replace('hidden', 'active-slide');
+    }
+}
+
